2114a918de962fa7ad4165009417b78fa428a6e0
saba756 <sabaofficial12@gmail.com>
2020-07-05 21:56:36 +0500

    intial commit

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext>options) : base(options)
+        {
+                
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
